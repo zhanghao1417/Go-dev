91875072ab7ffccc1987e14c11457f5a75e36955
zhanghao <zhanghao@51cto.com>
2020-07-21 11:35:41 +0800

    2020/07/21

---
+++ code/block2/boiling.go
@@ -0,0 +1,12 @@
+// Output: boiling point = 212°F or 100°C
+package main
+
+import "fmt"
+
+const boilingF = 212.0
+
+func main() {
+	var f = boilingF
+	var c = (f - 32) * 5 / 9
+	fmt.Printf("boiling point = %g°F or %g°C\n", f, c)
+}
